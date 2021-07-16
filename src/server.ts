64933ad844485a1ae76fa496cbc628cd26d39ee7
wellpinho <wellpinho@outlook.com>
2021-07-15 23:31:16 -0300

    Criado rotas com os métodos GET, POST, PUT e DELETE

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,22 @@
-import express from 'express';
+import express from 'express'
 
 const app = express()
+
+app.get('/', (req , res) => {
+  return res.send('Olá GET')
+})
+
+app.post('/post', (req, res) => {
+  return res.send('Olá POST')
+})
+
+app.put('/put', (req, res) => {
+  return res.send('Olá PUT')
+})
+
+app.delete('/delete', (req, res) => {
+  return res.send('Olá DELETE')
+})
 
 app.listen(4000, () => {
   console.log('Running on port 4000')
