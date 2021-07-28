dcfc74ff075257d63595ebd791b7bc6a530cf418
wellpinho <wellpinho@outlook.com>
2021-07-28 19:27:46 -0300

    Feito tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import 'reflect-metadata'
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
+import "express-async-errors"
 import './database'
 import router from './routes'
 
@@ -8,6 +9,23 @@ const app = express()
 app.use(express.json());
 
 app.use(router);
+
+app.use((
+  err: Error, 
+  req: Request, 
+  res: Response, 
+  next: NextFunction) => {
+    if (err instanceof Error) {
+      res.status(400).json({
+        error: err.message
+      })
+    }
+
+    return res.status(500).json({
+      status: 'Error',
+      message: 'Internal Server Error'
+    })
+})
 
 app.listen(4000, () => {
   console.log('Running on port 4000')
